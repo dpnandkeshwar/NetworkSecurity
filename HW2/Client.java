ead29861d2359912964b5dfcaebc4f262ab8966f
Dhanpaul Nandkeshwar <dpnandkeshwar@Dhanpauls-MacBook-Pro.local>
2022-02-05 18:53:09 -0700

    Finalize

--- HW2/Client.java
+++ HW2/Client.java
@@ -5,48 +5,58 @@ import java.math.BigInteger;
 import java.net.*;
 
 public class Client {
-	
+
 	private Socket socket;
 	private DataOutputStream oStream;
 	private DataInputStream iStream;
-	
+	private DataInputStream socketReader;
+
 	private BigInteger aSecret = new BigInteger("160031");
 	private BigInteger g = new BigInteger("1907");
 	private BigInteger p = new BigInteger("784313");
 	private BigInteger T_a;
 	private BigInteger T_b;
 	private BigInteger DFValue;
-	
+
 	public Client(String ipAddress, int port) throws UnknownHostException, IOException {
 		socket = new Socket(ipAddress, port);
 		System.out.println("Connected to " + ipAddress);
 		oStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 		iStream = new DataInputStream(new BufferedInputStream(System.in));
-		
-		String inputBuf = "";
+		socketReader = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+		String systemInBuf = "";
+		String socketInBuf = "";
 		T_a = DiffieHellman.calculateDiffieHellmanValue(g, aSecret, p);
-		
+
 		System.out.println("Alice's Calculation: " + T_a.toString());
+
+		oStream.writeChars(T_a.toString());
 		
-		while(!inputBuf.equals("QUIT")) {
-			inputBuf = iStream.readLine();
-			T_b = new BigInteger(inputBuf);
-			DFValue = efficientExponentiation.calculate(T_b, aSecret, p);
-			oStream.writeChars(T_a.toString());
-		}
+		System.out.println("Waiting for Bob to respond with his value...");
+
+		socketInBuf = iStream.readUTF();
+		T_b = new BigInteger(socketInBuf);
+		DFValue = efficientExponentiation.calculate(T_b, aSecret, p);
+		
+		System.out.println("Diffie Hellman value calculated. Type QUIT to exit this connection");
+		
+		while(!systemInBuf.equals("QUIT"))
+			systemInBuf = iStream.readUTF();
 		
 		System.out.println("Calculated Diffie Hellman Shared Key: " + DFValue.toString());
+		
+		oStream.writeChars("QUIT");
 		
 		try {
 			socket.close();
 			iStream.close();
 			oStream.close();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	
+
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		Client client = new Client("127.0.0.1", 8000);
 	}
--- HW2/Server.java
+++ HW2/Server.java
@@ -10,53 +10,56 @@ public class Server {
 	private ServerSocket serverSocket;
 	private DataInputStream iStream;
 	private DataOutputStream oStream;
-	
+
 	private BigInteger bSecret = new BigInteger("12077");
 	private BigInteger g = new BigInteger("1907");
 	private BigInteger p = new BigInteger("784313");
 	private BigInteger T_a;
 	private BigInteger T_b;
 	private BigInteger DFValue;
-	
-	public Server(int port) throws IOException {
+
+	public Server(int port) throws IOException, InterruptedException {
 		serverSocket = new ServerSocket(port);
 		System.out.println("Server started");
 		System.out.println("Waiting for client connection...");
-		
+
 		socket = serverSocket.accept();
 		System.out.println("Client accepted!");
-		
-		iStream = new DataInputStream(
-                new BufferedInputStream(socket.getInputStream()));
-		
-		oStream = new DataOutputStream(
-				new BufferedOutputStream(socket.getOutputStream()));
-		
+
+		iStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+		oStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
 		T_b = DiffieHellman.calculateDiffieHellmanValue(g, bSecret, p);
-		
+
 		System.out.println("Bob's Calculation: " + T_b.toString());
-		
+
 		String inputBuf = "";
 		
-		while(!inputBuf.equals("QUIT")) {
-			inputBuf = iStream.readUTF();
-			T_a = new BigInteger(inputBuf);
-			DFValue = efficientExponentiation.calculate(T_a, bSecret, p);
-			oStream.writeChars(T_b.toString());
-		}
+		System.out.println("Waiting for Alice to respond with her value...");
+
+		inputBuf = iStream.readUTF();
+		T_a = new BigInteger(inputBuf);
+		DFValue = efficientExponentiation.calculate(T_a, bSecret, p);
+		oStream.writeChars(T_b.toString());
 		
+		System.out.println("Awaiting client to close connection...");
+		
+		while(!iStream.readUTF().equals("QUIT"))
+			
+		Thread.sleep(2000);
+
 		try {
 			socket.close();
 			serverSocket.close();
 			iStream.close();
 			oStream.close();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
 		Server server = new Server(8000);
 	}
 }
